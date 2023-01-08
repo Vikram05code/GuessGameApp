81a4f1da02785ce1bf7bd679b549706e0d97a0f8
Vikram <vikky5vikram@gmail.com>
2023-01-08 18:55:14 +0530

    First Commit

---
+++ GuesserGameApp/src/com/vikram/GuesserGame.java
@@ -0,0 +1,17 @@
+package com.vikram;
+import java.util.Scanner;
+class Guesser{
+	int guessNum;
+	
+	int guessNum() {
+		Scanner sc = new Scanner(System.in);
+		guessNum = sc.nextInt();
+		return guessNum;
+	}
+}
+
+public class GuesserGame {
+   public static void main(String[] args) {
+	   
+   }
+}
